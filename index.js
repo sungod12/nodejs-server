f919918ab25ee814b48c922e5e2a58065770bdec
Suraj Prabhu <prabhusuraj103@gmail.com>
2021-05-21 22:02:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ const firebaseConfig = {
 };
 
 const express = require("express");
+const cors=require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors());
 const PORT=3001;
 const { encrypt, decrypt } = require("./EncryptionHandler");
 const firebase = require("firebase");
