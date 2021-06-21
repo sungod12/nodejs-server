e82b5be220b92d181302d458641fe5276688aecc
Suraj Prabhu <prabhusuraj103@gmail.com>
2021-06-21 11:01:44 +0530

    added welcome message

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
-const cors=require("cors");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
-const PORT=3001;
+const PORT = 3001;
 const { encrypt, decrypt } = require("./EncryptionHandler");
-const {database} = require("./fire");
+const { database } = require("./fire");
 
-
+app.get("/", (req, res) => {
+  res.send("Welcome to server");
+});
 
 app.post("/addPassword", (req, res) => {
   const db = database.ref(`${req.body.id}`);
@@ -19,33 +21,34 @@ app.post("/addPassword", (req, res) => {
   };
   db.push(details)
     .then(() => console.log("successfully inserted"))
-    .catch((err) => {console.log(err.message)});
+    .catch((err) => {
+      console.log(err.message);
+    });
   db.off();
 });
 
-app.get("/showPasswords/:id",async(req,res)=>{
-  id=req.params['id'];
+app.get("/showPasswords/:id", async (req, res) => {
+  id = req.params["id"];
   const db = database.ref(`${id}`);
-  const data=await db.get();
-  const value=data.val();
-  const temp=[];
-  for(let id in value){
-    temp.push({id,...value[id]});
+  const data = await db.get();
+  const value = data.val();
+  const temp = [];
+  for (let id in value) {
+    temp.push({ id, ...value[id] });
   }
   db.off();
   res.send(temp);
-  
-})
+});
 
-app.post("/deletePassword/:id",(req,res)=>{
-  id=req.params['id'];
+app.post("/deletePassword/:id", (req, res) => {
+  id = req.params["id"];
   const db = database.ref(`${id}/${req.body.id}`);
   db.remove();
   db.off();
-})
+});
 
-app.post("/decryptPassword",(req,res)=>{
+app.post("/decryptPassword", (req, res) => {
   res.send(decrypt(req.body));
-})
+});
 
-app.listen(process.env.PORT || PORT,()=>console.log("server is running"));
+app.listen(process.env.PORT || PORT, () => console.log("server is running"));
